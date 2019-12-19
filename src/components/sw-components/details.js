a3dd00ad6b93397db4c98802f4a60b3a5b1dd48f
dmitriyPC <dmitrypn81@gmail.com>
2019-12-19 21:39:06 +0300

    finishing and  routing

---
+++ src/components/sw-components/details.js
@@ -0,0 +1,21 @@
+
+
+
+const PersonDetails = () => {
+
+};
+
+const PlanetDetails = () => {
+	
+};
+
+const StarshipDetails = () => {
+	
+};
+
+
+export {
+	PersonDetails,
+	PlanetDetails,
+	StarshipDetails
+};
