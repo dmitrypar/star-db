9d34ae11f9eff79214a1ce4bed9384a30a8c877d
dmitriyPC <dmitrypn81@gmail.com>
2019-12-24 15:25:11 +0300

    finish part on JS+redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
-const reduser = (state=0, action) => {
+import {createStore} from 'redux';
 
-	switch(action.type) {
-case 'INC':
-return state + 1;
-default:
-return state;
 
+const reducer = (state=0, action) => {
+
+switch(action.type) {
+	case 'INC':
+	return state + 1;
+
+	case 'DEC':
+	return state - 1;
+
+	default:
+	return state;
 	}
-
 };
 
-let state = reduser(undefined, {})
+const store = createStore(reducer);
 
-state = reduser(state, {type:'INC'});
+document.getElementById('inc').addEventListener('click', ()=>{
+	store.dispatch({type: 'INC'});
+});
+
+
+document
+.getElementById('dec')
+.addEventListener('click', ()=>{
+	store.dispatch({type: 'DEC'});
+});
+
+const update = ()  => {
+document
+.getElementById('counter')
+.innerHTML= store.getState()
+};
+
+store.subscribe(update);
+
+
+
+/*let state = reducer(undefined, {})
+
+state = reducer(state, {type:'INC'});
 console.log(state);
 
-state = reduser(state, {type:'INC'});
+state = reducer(state, {type:'INC'});
 
-console.log(state);
+console.log(state);*/
+
+
+
+/*store.subscribe(()=>{
+	console.log(store.getState());
+});
+
+console.log(store.getState());
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});*/
